e8cd8212624d247dc50e332b98ae6f75be243cc0
Nadia-Anahi-Garcia <nadia.garcia@tupad.utn.edu.ar>
2025-08-26 16:44:13 -0300

    Ejercicio1

---
+++ Unidad 3 - Estructuras Condicionales/GARCIA_Tp3.py
@@ -0,0 +1,8 @@
+#Ejercicio 1 : 
+#Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años, 
+#deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+
+edad = int(input("Por favor, ingrese su edad:"))
+edadlimite=18
+if edad >= edadlimite:
+    print("Eres mayor de edad")
