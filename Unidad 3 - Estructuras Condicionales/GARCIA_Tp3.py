1ae380663c355665aa6ff57c6c8846301c3efa40
Nadia-Anahi-Garcia <nadia.garcia@tupad.utn.edu.ar>
2025-08-28 10:51:54 -0300

    Modificación y finalización de ejercicios

--- Unidad 3 - Estructuras Condicionales/GARCIA_Tp3.py
+++ Unidad 3 - Estructuras Condicionales/GARCIA_Tp3.py
@@ -29,8 +29,10 @@ nota = int(input("Por favor, ingrese su nota (0 a 10): "))
 #Mostramos por pantalla si esta aprobado o desaprobado.
 if (nota < 0) or (nota > 10):
     print("Por favor, ingrese un número del 0 al 10.")
-else: 
-    print("Aprobado" if nota >= 6 else "Desaprobado")
+elif nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
 
 #Ejercicio 3: 
 #Escribir un programa que permita ingresar solo números pares. 
@@ -63,9 +65,9 @@ if edad2 < 0:
     print ("Ingrese una edad valida.")
 elif edad2 < 12:
     print("Perteneces a la categoría Niño/a.")
-elif (edad2 >=12) and (edad2 < 18):
+elif edad2 < 18:
     print("Perteneces a la categoría Adolescente.")
-elif (edad2 >= 18) and (edad2 < 30):
+elif edad2 < 30:
     print("Perteneces a la categoría Adulto/a joven.")
 else:
     print("Perteneces a la categoría Adulto/a.")
@@ -89,6 +91,40 @@ if (validacion >= 8) and (validacion  <= 14):
 else:
     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres.")
 
+#Ejercicio 6
+#Escribir un programa que tome la lista numeros_aleatorios, calcule su moda, su mediana
+#  y su media y las compare para determinar si hay 
+# sesgo positivo, negativo o no hay sesgo. Imprimir el resultado por pantalla.
+
+#Primero importamos la funsión de media, mediana y moda según material provisto. 
+from statistics import mean, median, mode
+#Importamos los números aleatorios según material provisto.
+import random
+numeros_aleatorios = [random.randint(1,100)for i in range (50)]
+
+#Cálculamos cada una
+media = mean(numeros_aleatorios)
+mediana = median(numeros_aleatorios)
+moda = mode(numeros_aleatorios)
+
+#Mostramos por pantalla cada una y también los números aleatorios.
+print(f"Los números son: {numeros_aleatorios}")
+print(f"La media: {media:.2f}")
+print(f"La mediana: {mediana:.2f}")
+print(f"La moda: {moda:.2f}")
+
+#Luego comparamos según como estaba en el pdf la definisión de cada sosego. 
+if media > mediana > moda:
+    print ("Hay sosego positivo o a la derecha.")
+elif media < mediana < moda:
+    print ("Hay sosego negativo o a la izquierda.")
+elif media == mediana == moda:
+    print ("No hay sosego.")
+else:
+    print("El sosego no está definido.")
+
+
+
 
 #Ejercicio 7
 #Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado termina con vocal, 
@@ -105,7 +141,7 @@ ultima_letra = palabra[-1]
 vocales = ["a","e","i","o","u"]
 
 #Evaluamos y mostramos.
-if ultima_letra.lowe() in vocales:
+if ultima_letra.lower() in vocales:
     print(f"{palabra}!")
 else:
     print (palabra)
@@ -120,7 +156,7 @@ else:
 #El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada 
 # por el usuario e imprimir el resultado por pantalla
 
-
+#Solicitamos que el usuario ingrese su nombre y la opción.
 nombre1 = input("Ingrese su nombre:")
 num = int(input(
     "Ingrese un número según la opción deseada:\n"
@@ -129,6 +165,7 @@ num = int(input(
     "3. Nombre con la primera letra en mayúscula\n"
     "Opción: "))
 
+#Validamos el ingreso de las opciones pedida y evaluamos la condición, cuando haya ingresado bien el número. 
 if (num >= 1)  and (num <= 3):
     if num == 1:
         print(nombre1.upper())
@@ -151,19 +188,22 @@ else:
 #● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
 #● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
 
+#Solicitamos el ingreso de la magnitud del terremoto
 magnitud_terremoto = float(input("Ingrese la magnitud del terremoto: "))
 
+#Validamos que no ingrese un núero negativo y evaluamos la condición en caso de que ingrese 
+#Un número correcto. 
 if magnitud_terremoto <= 0: 
     print ("Por favor, ingrese un número correcto.")
 elif magnitud_terremoto < 3:
     print("Muy leve (imperceptible)")
-elif (magnitud_terremoto >= 3 ) and (magnitud_terremoto < 4):
+elif magnitud_terremoto < 4:
     print("Leve (ligeramente perceptible).")
-elif (magnitud_terremoto >= 4) and (magnitud_terremoto < 5):
+elif magnitud_terremoto < 5:
     print ("Moderado (sentido por personas, pero generalmente no causa daños).")
-elif (magnitud_terremoto >= 5) and (magnitud_terremoto < 6): 
+elif magnitud_terremoto < 6: 
     print ("Fuerte (puede causar daños en estructuras débiles).")
-elif (magnitud_terremoto >= 6) and (magnitud_terremoto < 7): 
+elif magnitud_terremoto < 7: 
     print ("Muy Fuerte (puede causar daños significativos).")
 else: 
     print ("Extremo (puede causar graves daños a gran escala).")
@@ -174,7 +214,46 @@ else:
 # qué mes del año es y qué día es. El programa deberá utilizar esa información 
 # para imprimir por pantalla si el usuario se encuentra en otoño, invierno, primavera o verano. 
 
-    
+#Solicitamos a la persona que ingrese en que hemisferio está y lo pasamos todo a mayúscula. 
+hemisferio = input("¿En cuál hemisferio se encuentra (N / S)? ").upper()
+
+#Validamos que no ingrese un dato mal y vamos evaluando según lo que ingrese. 
+if (hemisferio == "N") or (hemisferio == "S"):
+    #Si ingresa bien el hemisferio, le solicita que ingrese el mes y pasamos todo a minúscula. 
+    #También creamos una lista con los meses de año válidos. 
+    mes = input("Ingrese el mes: ").lower()
+    meses_anio = ["enero","febrero","marzo","abril","mayo","junio", "julio","agosto","septiembre","octubre","noviembre","diciembre"]
+    #Si ingresa bien el mes (está dentro de la lista creada), solictamos el día. 
+    if mes in meses_anio:
+        dia = int(input("Ingrese el día del mes: "))
+        #Se colocó 1 a 31 ya que si ingresa otro número por fuera de lo tiene máximo un mes el programa le pide
+        #que ingrese un día válido. 
+        if 1 <= dia <= 31:
+            # Si está bien, evaluamos la estación y guardamos en la variable según lo que el usuario ingrese. 
+            if ((mes == "diciembre") and (dia >= 21)) or (mes in ["enero", "febrero"]) or ((mes == "marzo") and (dia <= 20)):
+                estacion_n = "invierno"
+                estacion_s = "verano"
+            elif ((mes == "marzo") and (dia >= 21)) or (mes in ["abril", "mayo"]) or ((mes == "junio") and (dia <= 20)):
+                estacion_n = "primavera"
+                estacion_s = "otoño"
+            elif ((mes == "junio")and (dia >= 21)) or (mes in ["julio", "agosto"]) or ((mes == "septiembre") and (dia <= 20)):
+                estacion_n = "verano"
+                estacion_s = "invierno"
+            elif ((mes == "septiembre") and (dia >= 21)) or (mes in ["octubre", "noviembre"]) or ((mes == "diciembre") and (dia <= 20)):
+                estacion_n = "otoño"
+                estacion_s = "primavera"
+                # Mostramos el resultado segúl el hemisferio. 
+            if hemisferio == "N":
+                print(f"Estás en {estacion_n}")
+            else:
+                print(f"Estás en {estacion_s}")
+        else:
+            print(" Ingrese un día válido (1-31).")
+    else:
+        print("Por favor, ingrese un mes válido.")
+else:
+    print("Por favor, ingrese Por favor, ingrese S (hemisferio sur) o N (hemisferio norte).")
+
 
 
 
