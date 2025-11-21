f5ed9dae5b4b03df4a8043d8dca341cc9b045b23
Nadia-Anahi-Garcia <nadia.garcia@tupad.utn.edu.ar>
2025-11-21 20:16:34 -0300

    corrección del 6

--- Unidad 5 - Listas/GRACIA_TP5.py
+++ Unidad 5 - Listas/GRACIA_TP5.py
@@ -189,7 +189,7 @@ lista_numeros = [10, 20, 30, 40, 50, 60, 70]
 # Guardamos el último
 ultimo = lista_numeros[-1]
 # Desplazamos cada elemento una posición a la derecha
-for i in range((lista_numeros) -1, 0, -1):
+for i in range(len(lista_numeros) -1, 0, -1):
     lista_numeros[i] = lista_numeros[i - 1]
 
 # Ponemos el último al inicio
